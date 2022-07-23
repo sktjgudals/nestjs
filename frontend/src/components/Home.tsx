7951f880103142fc370452997a2d0fea7cea3132
82109@DESKTOP-CP7KARN <81937738+sktjgudals@users.noreply.github.com>
2022-07-24 00:24:08 +0900

    [UPDATE] frontend title

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -3,14 +3,13 @@ import "./style.css";
 import { Link } from "react-router-dom";
 import { FiAlignLeft } from "react-icons/fi";
 import List from "./List";
+import Header from "./Header";
 
 const Home: React.FC = () => {
   const url = `http://localhost:4000/to-do/`;
   return (
     <>
-      <Link to="/" className="heading">
-        All ToDoList
-      </Link>
+      <Header path={"/"} title={`All ToDoList`} />
       <Link to="/daylist" className="icon_heading">
         <FiAlignLeft />
       </Link>
